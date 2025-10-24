fcf3f9eb581ebe0265e970d2d9584ca71e67eff1
jaffersadiqjs <jaffersadiq2697@gmail.com>
2025-10-24 20:51:41 +0530

    Update settings.py

--- food_ordering/settings.py
+++ food_ordering/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vvjb5mc=_q9hkwz#c$clnlv0&1v1bcm4mptyv+dfyeez(hq0ww'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
